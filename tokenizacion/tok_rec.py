6df622d5a678b5295f8e9280df98bb60c3129ceb
Bruno Zabala <brunozabalap@gmail.com>
2025-03-01 18:01:53 -0600

    Tokenization

--- tokenizacion/tok_rec.py
+++ tokenizacion/tok_rec.py
@@ -34,29 +34,35 @@ def tokenize(dict_regex, text, token_list):
 
         else:
             print("Error, no match found for ", text)
-            return token_list
+            return token_list, False
 
     else:
         # print("String ended")
-        return token_list
+        return token_list, True
 
 dict_regex = {"for": r"^for\b",
             "if": r"^if\b",
             "else": r"^else\b",
+            "print": r"^print\b",
+            "with": r"^with\b",
+            "open": r"^open\b",
+            "in": r"^in\b",
             "while": r"^while\b",
             "def": r"^def\b",
             "return": r"^return\b",
             "as": r"^as\b",
+            "not": r"^not\b",
             "import": r"^import\b",
+            "enumerate": r"^enumerate\b",
             "method": r"^\.[a-zA-Z]\w*\b",
             "int": r"^-?[0-9]+\b",
             "float": r"^-?[0-9]+\.[0-9]+\b",
-            "string": r"^\".*\"",
+            "string": r"^\"(\\.|[^\"])*\"",
             "real_num": r"^-?[0-9]+\.[0-9]E?-?[0-9]+\b",
             "identifier": r"^[a-zA-Z]\w*\b",
             "comment": r"^//.*",
             "python_comment": r"^#.*",
-            "op_asign": r"^=",
+            "op_assign": r"^=",
             "op_compare": r"^==",
             "op_less": r"^<",
             "op_greater": r"^>",
@@ -80,10 +86,23 @@ dict_regex = {"for": r"^for\b",
 
 test_file = open("test.txt", "r")
 lines = test_file.readlines()
+line_num = 1
+error_lines = []
 
 for line in lines:
     token_list = []
 
     if line.strip():
-        print(line.strip())
-        print(tokenize(dict_regex, line, token_list), "\n")
+        result = tokenize(dict_regex, line, token_list)
+
+        print(f"Line {line_num}: {line.strip()}")
+        print(result[0], "\n")
+
+        if not result[1]:
+            error_lines.append(line_num)
+        
+    line_num += 1
+
+print("Errors found in lines: ")
+for line in error_lines:
+    print(line)
